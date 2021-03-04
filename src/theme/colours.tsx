4810b2b9da7257abb1580fbb142814c6de6351aa
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-03-04 18:58:19 -0300

    Adds theme colours

---
+++ src/theme/colours.tsx
@@ -0,0 +1,16 @@
+// Text and element
+const kMainColour = '#FA4A0C';
+const kBlackColour = '#000000';
+const kWhiteColour = '#FFFFFF';
+const kDisabledColour = '#C7C7C7';
+
+// Body
+const kBackgroundColour = '#F2F2F2';
+
+export default {
+  kMainColour,
+  kBlackColour,
+  kWhiteColour,
+  kBackgroundColour,
+  kDisabledColour,
+};
