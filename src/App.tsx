4e65056e7a3809caf4de0f7deb3dd2cf03644183
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2021-03-13 18:24:29 -0300

    feat: add stack navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import Onboarding from './screens/unauthed/Onboarding';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const {Navigator, Screen} = createStackNavigator();
 
 const App = () => {
   return (
-    <>
-      <Onboarding.component />
-    </>
+    <NavigationContainer>
+      <Navigator
+        initialRouteName={Onboarding.name}
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Screen name={Onboarding.name} component={Onboarding.component} />
+      </Navigator>
+    </NavigationContainer>
   );
 };
 
